a6aadb4ac81ad5e5bd7ec350b20dd702dc37b9ff
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-19 20:32:21 +0100

    Added loadRobotsThunk thunk

--- src/redux/thunks/loadRobotsThunk.js
+++ src/redux/thunks/loadRobotsThunk.js
@@ -1,4 +1,11 @@
-const loadRobotsThunk () =>{
-const apiURL = process.env.REACT_APP_API_URL;
-}
-export default loadRobotsThunk
+import loadRobotsAction from "../actions/actionCreators";
+
+const loadRobotsThunk = async (dispatch) => {
+  const apiURL = process.env.REACT_APP_API_URL;
+  const response = await fetch(apiURL);
+  const robots = await response.json();
+  if (response.ok) {
+    dispatch(loadRobotsAction(robots));
+  }
+};
+export default loadRobotsThunk;
