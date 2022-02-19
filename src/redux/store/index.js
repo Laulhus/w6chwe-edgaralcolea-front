5bb9b5427f6d4e6302a1623238235faacb68a07f
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-19 20:28:51 +0100

    Added .env file

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,7 +1,7 @@
 import { composeWithDevTools } from "@redux-devtools/extension";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import rootReducer from "../reducers";
 
 const store = createStore(
   rootReducer,
---
+++ src/redux/thunks/loadRobotsThunk.js
@@ -0,0 +1,4 @@
+const loadRobotsThunk () =>{
+const apiURL = process.env.REACT_APP_API_URL;
+}
+export default loadRobotsThunk
