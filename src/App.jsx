94061797af0e51e9dc07493384103e3ccb776fed
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-19 18:12:06 +0100

    Created NavComponent.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React from "react";
+import NavComponent from "./components/Nav/NavComponent";
 
 function App() {
   return (
     <div className="container-fluid">
       <div className="header">
-        <ul className="nav row align-items-center justify-content-center navbar__list">
-          <li className="col-4 nav-link navbar__icon">Test</li>
-          <li className="col-4 nav-link navbar__icon">Test</li>
-          <li className="col-4 nav-link navbar__icon">Test</li>
-        </ul>
+        <NavComponent />
         <div className="main row justify-content-center align-items-center">
-          <div className="grid col-12">
-            <ul className="col-3 robot-list list-unstyled">
+          <div className="grid col-12 ">
+            <ul className="robot-list list-unstyled">
               <li className="list-item robot-card">Test Robot</li>
             </ul>
           </div>
