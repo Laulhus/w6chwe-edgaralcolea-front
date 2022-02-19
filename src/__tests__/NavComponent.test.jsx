72798039ea58b639ea4b6948b9901cd13f8e5907
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-20 00:43:23 +0100

    created renderWithProviders function and refactor tests

--- src/__tests__/NavComponent.test.jsx
+++ src/__tests__/NavComponent.test.jsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import NavComponent from "../components/Nav/NavComponent";
+import renderWithProviders from "../setupTests";
 
 describe("Given a NavComponent component", () => {
   describe("When instantiated", () => {
     test("Then it should render 3 list items", () => {
-      render(<NavComponent />);
+      renderWithProviders(<NavComponent />);
 
       const listItems = screen.getAllByRole("listitem");
 
--- src/__tests__/RobotComponent.test.jsx
+++ src/__tests__/RobotComponent.test.jsx
@@ -1,11 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import React from "react";
 import RobotComponent from "../components/RobotComponent/RobotComponent";
+import renderWithProviders from "../setupTests";
 
 describe("Given a RobotComponent component", () => {
   describe("When instantiated", () => {
     test("Then it should render an image", () => {
-      render(<RobotComponent />);
+      const robot = {
+        name: "Tardis",
+        speed: 8,
+        endurance: 10,
+        img: "testUrl",
+        creationDate: "10-01-2000",
+      };
+
+      renderWithProviders(<RobotComponent robot={robot} />);
 
       const element = screen.getByRole("img");
 
