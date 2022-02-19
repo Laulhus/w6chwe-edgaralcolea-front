3838b2b362881544e9e850dd7aa6f87b47d0190a
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-19 19:49:38 +0100

    Tested loadRobotsAction

---
+++ src/__tests__/actionCreators.test.js
@@ -0,0 +1,25 @@
+import loadRobotsAction from "../redux/actions/actionCreators";
+
+describe("Given a loadCharacterAction function", () => {
+  describe("When called with an array of robots", () => {
+    test('Then it should return an action object with type: "load-robots" and the array', () => {
+      const robots = [
+        {
+          name: "Tardis",
+          speed: 8,
+        },
+        {
+          name: "Cybermen",
+          speed: 2,
+        },
+      ];
+      const expectedAction = {
+        type: "load-robots",
+        robots,
+      };
+      const action = loadRobotsAction(robots);
+
+      expect(action).toEqual(expectedAction);
+    });
+  });
+});
