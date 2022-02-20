89ff533936b2a1774afc359dc8ff24948ce105e7
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-21 00:07:00 +0100

    Renamed App and createRobot tests to .test.js

---
+++ src/__tests__/createRobotPage.test.js
@@ -0,0 +1,20 @@
+import { screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import CreateRobotPage from "../pages/CreateRobotPage";
+
+import renderWithProviders from "../setupTests";
+
+describe("Given an CreateRobotPage component", () => {
+  describe("When rendered", () => {
+    test("Then it should render a form", () => {
+      renderWithProviders(
+        <BrowserRouter>
+          <CreateRobotPage />
+        </BrowserRouter>
+      );
+
+      const element = screen.getByRole("button");
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
