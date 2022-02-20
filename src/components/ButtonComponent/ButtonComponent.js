29d0e22a40b20be8e0add6307bb30b935c5d6fa5
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-20 13:36:41 +0100

    Deleted ButtonComponent

--- src/components/ButtonComponent/ButtonComponent.js
+++
@@ -1,6 +0,0 @@
-const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome");
-
-const ButtonComponent = (actionOnClick) => {
-  return <FontAwesomeIcon icon="fa-solid fa-trash-can" />;
-};
-export default ButtonComponent;
