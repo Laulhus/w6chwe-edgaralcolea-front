4bc3ed2eaf4fa5ffa0ff5b06df8e16723a33ed65
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-20 21:38:48 +0100

    Created form for robot creation

--- src/pages/CreateRobotPage.js
+++ src/pages/CreateRobotPage.js
@@ -1,12 +1,82 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
 const CreateRobotPage = () => {
+  const blankForm = {
+    name: "",
+    img: "",
+    speed: "",
+    endurance: "",
+    creationDate: "",
+  };
+
+  const [formData, setFormData] = useState(blankForm);
+
+  const isFormInvalid =
+    formData.name === "" ||
+    formData.img === "" ||
+    formData.speed === "" ||
+    formData.endurance === "" ||
+    formData.creationDate === "";
+
+  const changeData = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const dispatch = useDispatch();
+
   return (
     <div className="header">
       <ul className="nav row align-items-center justify-content-center navbar__list">
         <li className="col-4 d-flex justify-content-center nav-link navbar__icon"></li>
       </ul>
       <div className="main row justify-content-center align-items-center">
-        <div className="grid row align-items-center justify-content-center ">
-          <form action=""></form>
+        <div className="form-container col-8">
+          <form noValidate autoComplete="off">
+            <div className="form">
+              <label htmlFor="name">Name:</label>{" "}
+              <input
+                type="text"
+                id="name"
+                value={formData.name}
+                onChange={changeData}
+              />
+              <label htmlFor="img">Image Url:</label>{" "}
+              <input
+                type="text"
+                id="img"
+                value={formData.img}
+                onChange={changeData}
+              />
+              <label htmlFor="speed">Speed:</label>{" "}
+              <input
+                type="number"
+                id="speed"
+                value={formData.speed}
+                onChange={changeData}
+              />
+              <label htmlFor="endurance">Endurance:</label>{" "}
+              <input
+                type="number"
+                id="endurance"
+                value={formData.endurance}
+                onChange={changeData}
+              />
+              <label htmlFor="creationDate">Created on:</label>{" "}
+              <input
+                type="text"
+                id="creationDate"
+                value={formData.creationDate}
+                onChange={changeData}
+              />
+            </div>
+            <button type="submit" disabled={isFormInvalid}>
+              Create!
+            </button>
+          </form>
         </div>
       </div>
     </div>
