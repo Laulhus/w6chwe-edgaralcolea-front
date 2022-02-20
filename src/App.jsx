1d05d5385cc0674256fef5ab13c08496da00c947
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-20 17:49:38 +0100

    Installed react-router-dom and added Link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { Link, Router } from "react-router-dom";
 import NavComponent from "./components/NavComponent/NavComponent";
 import RobotComponent from "./components/RobotComponent/RobotComponent";
 import deleteRobotThunk from "./redux/thunks/deleteRobotThunk";
@@ -19,7 +20,11 @@ function App() {
   return (
     <div className="container-fluid">
       <div className="header">
-        <NavComponent />
+        <ul className="nav row align-items-center justify-content-center navbar__list">
+          <li className="col-4 d-flex justify-content-center nav-link navbar__icon">
+            <Link to="/character-matches">Create Robot</Link>
+          </li>
+        </ul>
         <div className="main row justify-content-center align-items-center">
           <div className="grid row align-items-center justify-content-center ">
             <ul className="col-8 robot-list d-flex justify-content-around">
