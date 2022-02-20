316dacc3ee6dabbc28d0077936e92e3bcbfc8b49
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-20 13:44:12 +0100

    Fixed unused var in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
