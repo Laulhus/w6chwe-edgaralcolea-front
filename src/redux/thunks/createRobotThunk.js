a11cd27b7197d679c64f3faebebdad477bc4fd5f
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-20 22:01:13 +0100

    Created createRobotThunk.js

---
+++ src/redux/thunks/createRobotThunk.js
@@ -0,0 +1,17 @@
+import { createRobotAction } from "../actions/actionCreators";
+
+const createRobotThunk = (robot) => async (dispatch) => {
+  const apiURL = process.env.REACT_APP_API_URL;
+  const response = await fetch(`${apiURL}create`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(robot),
+  });
+  if (response.ok) {
+    dispatch(createRobotAction(robot));
+  }
+};
+
+export default createRobotThunk;
