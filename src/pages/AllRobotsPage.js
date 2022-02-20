b905f9501fd80a4a0d9e525310e712e4e226f979
Edgar Alcolea <edgaralcolea@gmail.com>
2022-02-20 18:56:55 +0100

    Created AllRobotsPage and RobotPage components

---
+++ src/pages/AllRobotsPage.js
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import RobotComponent from "../components/RobotComponent/RobotComponent";
+import deleteRobotThunk from "../redux/thunks/deleteRobotThunk";
+import loadRobotsThunk from "../redux/thunks/loadRobotsThunk";
+
+const AllRobotsPage = () => {
+  const dispatch = useDispatch();
+  const robots = useSelector((state) => state.robots);
+
+  useEffect(() => {
+    dispatch(loadRobotsThunk);
+  }, [dispatch]);
+
+  const navigate = useNavigate();
+  const goToPage = (id) => {
+    navigate(`/robot/${id}`);
+  };
+
+  const deleteRobot = (id) => {
+    dispatch(deleteRobotThunk(id));
+  };
+  return (
+    <div className="header">
+      <ul className="nav row align-items-center justify-content-center navbar__list">
+        <li className="col-4 d-flex justify-content-center nav-link navbar__icon"></li>
+      </ul>
+      <div className="main row justify-content-center align-items-center">
+        <div className="grid row align-items-center justify-content-center ">
+          <ul className="col-8 robot-list d-flex justify-content-around">
+            {robots.map((robot) => (
+              <RobotComponent
+                key={robot._id}
+                robot={robot}
+                actionOnClick={deleteRobot}
+                navigateClick={() => goToPage(robot._id)}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AllRobotsPage;
